0139ddad8457042f38a79e08171b86a45cbf501a
MochaCoral <90062849+MochaCoral@users.noreply.github.com>
2022-02-04 15:36:03 -0800

    first commit

---
+++ durkillesburgTester.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class durkillesburgTester {
+    public class durkillesburgTester {
+        @Test 
+        public void satisFactorOne() {
+            assertEquals(1000, durkillesburg.gregg("mae"));
+        }
+    }
+}
